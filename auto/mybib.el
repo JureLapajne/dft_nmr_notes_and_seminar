4c441bac8a7d45e939b3536d67f72d5222569450
Jure Lapajne <lapajne.jure@gmail.com>
2018-10-26 15:04:18 +0200

    dodani viri

---
+++ auto/mybib.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook
+ "mybib"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "advanced_course"
+    "cramer09_densit_funct_theor_trans_metal"
+    "charpentier11_paw_approac_comput_nmr_param"
+    "grimme10_consis_accur_ab_initio_param"))
+ :bibtex)
+
