ac61df973fca01702ad3b8aac2de9924e903a941
Jure Lapajne <lapajne.jure@gmail.com>
2018-11-22 14:24:53 +0100

    update

--- auto/mybib.el
+++ auto/mybib.el
@@ -9,6 +9,8 @@
     "mondal17_large_scale_comput_nuclear_magnet"
     "challenges_den_fun_theor"
     "perdew05_presc_desig_selec_densit_funct_approx"
-    "carchini13_how_theor_simul_can_addres"))
+    "comp_complexity"
+    "whitfield13_comput_compl_elect_struc"
+    "nobel_lecture"))
  :bibtex)
 
