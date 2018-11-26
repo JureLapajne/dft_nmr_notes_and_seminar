32409ae35ed5249af85d0f07eea67b4f5bddff30
Jure Lapajne <lapajne.jure@gmail.com>
2018-11-26 15:23:51 +0100

    levy-lieb extension explained, functionals, dispersion

--- auto/mybib.el
+++ auto/mybib.el
@@ -5,12 +5,14 @@
     "advanced_course"
     "cramer09_densit_funct_theor_trans_metal"
     "charpentier11_paw_approac_comput_nmr_param"
-    "grimme10_consis_accur_ab_initio_param"
+    "consis_accur_ab_initio_param"
     "mondal17_large_scale_comput_nuclear_magnet"
     "challenges_den_fun_theor"
     "perdew05_presc_desig_selec_densit_funct_approx"
-    "comp_complexity"
+    "How_theo_simul_can_address"
     "whitfield13_comput_compl_elect_struc"
-    "nobel_lecture"))
+    "nobel_lecture"
+    "birds_eye_view_densit_funct_theor"
+    "Density-functional_theory_of_atoms_and_molecules"))
  :bibtex)
 
