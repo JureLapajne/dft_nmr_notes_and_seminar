4bfb3f8b1ffa2aa25365f8e3311a47f80e73c1e5
Jure Lapajne <lapajne.jure@gmail.com>
2019-01-02 22:27:21 +0100

    fixes, some changes

---
+++ auto/mybib.el
@@ -0,0 +1,25 @@
+(TeX-add-style-hook
+ "mybib"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "advanced_course"
+    "cramer09_densit_funct_theor_trans_metal"
+    "charpentier11_paw_approac_comput_nmr_param"
+    "consis_accur_ab_initio_param"
+    "mondal17_large_scale_comput_nuclear_magnet"
+    "challenges_den_fun_theor"
+    "perdew05_presc_desig_selec_densit_funct_approx"
+    "How_theo_simul_can_address"
+    "whitfield13_comput_compl_elect_struc"
+    "nobel_lecture"
+    "birds_eye_view_densit_funct_theor"
+    "Density-functional_theory_of_atoms_and_molecules"
+    "deriv_lieb_defin_energ_funct"
+    "densit_funct_theor_elect_struc"
+    "orca"
+    "introd_to_metal_organ_frameworks"
+    "chemic_shift_tensor_review"
+    "Assignment_of_Solid_State"
+    "Dft_Investigation_of_the_Effect_of_Spin_Orbit"))
+ :bibtex)
+
