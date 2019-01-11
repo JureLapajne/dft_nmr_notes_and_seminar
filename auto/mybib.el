14a4d25b2058086af03a4526659fcdb3639991cb
Jure Lapajne <lapajne.jure@gmail.com>
2019-01-11 14:22:31 +0100

    small fixes

--- auto/mybib.el
+++ auto/mybib.el
@@ -8,7 +8,7 @@
     "consis_accur_ab_initio_param"
     "mondal17_large_scale_comput_nuclear_magnet"
     "challenges_den_fun_theor"
-    "perdew05_presc_desig_selec_densit_funct_approx"
+    "presc_desig_selec_densit_funct_approx"
     "How_theo_simul_can_address"
     "whitfield13_comput_compl_elect_struc"
     "nobel_lecture"
