2763aa725dd7e2cf9dde4b73180ba70538529065
Jure Lapajne <lapajne.jure@gmail.com>
2019-02-05 15:45:16 +0100

    started making presentation

--- predstavitev_ki/auto/predstavitev.el
+++ predstavitev_ki/auto/predstavitev.el
@@ -5,6 +5,14 @@
                      '(("beamer" "slovene" "usenames" "dvipsnames")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8x") ("fontenc" "T1") ("babel" "slovene") ("ragged2e" "document") ("hf-tikz" "customcolors" "norndcorners") ("epstopdf" "outdir=../uporabljene_slike/")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "beamer"
